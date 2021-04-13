17cc5660759425162d5ef81718a4b35a26395282
Daemon Wiest <wiestdaemon@gmail.com>
2021-04-13 09:32:02 -0700

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent } from './home/home.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HelpComponent,
+    HomeComponent,
+    UserDetailComponent,
+    UserListComponent,
+    MenuComponent,
+    
+
   ],
   imports: [
     BrowserModule,
